e21a9dce1c62ca51fcbb7d36036d7f4d93d680d5
Aminul islam <aminurrohman98@gmail.com>
2026-07-15 12:43:25 +0100

    feat: add mouseenter event listener to h1 for alerting user interaction

--- script.js
+++ script.js
@@ -81,3 +81,11 @@ btnScrollTo.addEventListener('click', function (e) {
     behavior: 'smooth',
   });
 });
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+};
+h1.addEventListener('mouseenter', alertH1);
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
