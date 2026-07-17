7d521514c4efa4a4946913ccb2c2f15b1fd72f24
Aminul islam <aminurrohman98@gmail.com>
2026-07-17 17:20:04 +0100

    Refactor sticky navigation logic and add comment for revealing elements on scroll

--- script.js
+++ script.js
@@ -199,7 +199,6 @@ window.addEventListener('scroll', function (e) {
 // const header = document.querySelector('.header');
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
   if (!entry.isIntersecting) nav.classList.add('sticky');
   nav.classList.add('sticky');
 };
@@ -209,3 +208,5 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+// Revealing Elements on scroll
